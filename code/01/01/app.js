dd8668aa3ee6554dcb4e646e8920f5e10cebf25a
shbrs-tech <shipilovb@gmail.com>
2025-10-21 13:04:18 +0300

    Update104

---
+++ code/01/01/app.js
@@ -0,0 +1,33 @@
+//alert('Tere tulemast');
+
+//confirm("Kas tahad louna minna");
+
+//let firstName = 'Boris';
+
+//firstName = 'Miki'
+
+//alert(firstName);
+
+//let firstName = prompt("Palun sisteta oma nimi");
+
+//alert(firstName);
+
+//let choice = confirm('Vali ei voi jah');
+//alert('confirm');
+
+//2 pop ups 2 adds 3 rd pop up names togerther 
+//const number1 = prompt ('Sisesta esimene number');
+//const number2 = prompt ('Sisestage teine number');
+//alert(number1 + " " + number2);
+
+//#added +1 to age 
+//let age = 30;
+//alert(age);
+//age += 1;
+// age = age +1;
+//alert(age)
+
+
+//pop up added number is true pop up true if no pop up false 
+const password = prompt ('Sisesta parool');
+alert (password == "123")
--- code/01/02/app.js
+++ code/01/02/app.js
@@ -1,2 +1,29 @@
+//tervituse programm
+/*
 const name =  prompt ( 'Mis Su nimi on ?');
-alert (name);
+alert ('Tere ' + name);
+*/
+
+/*const year = prompt ('Sisesta ome sunniaasta');
+const currentYear = 2025;
+
+alert ('Sinu vanus on:' + age);*/
+
+
+//my version 
+/*const year = prompt("Sisestahge one sunniaasta:");
+const currentYear = new Date().getFullYear();
+const age = currentYear - year;
+
+alert("Sinu vanus on : " + age);*/
+
+/*const length = Number(prompt("Sisestage ristkuliku pikkus :"));
+const width = Number(prompt("Sisestage ristkuliku laius:"));
+
+const area = length * width;
+alert("Ristküliku pindala on: " + area);*/
+
+const celsium  = Number (prompt('Sisestage Celsius'));
+const fahrenheit = celsium * 9/5+32;
+alert (celsium + " Celsius is" + fahrenheit + "Fahrenheit degrees");
+
