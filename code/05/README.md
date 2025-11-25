ed8c30b9f83721652c6172e30a4f9bbaba1f719d
shbrs-tech <shipilovb@gmail.com>
2025-11-25 11:43:34 +0200

    1_pauss

---
+++ code/05/README.md
@@ -0,0 +1,12 @@
+# Viimane loenu teemad 
+
+-Tegevused stringidega 
+    - liitmine
+    - toUpperCase()
+    - toLowerCase()
+    - includes()
+  - Sisendi kontrollimine
+    - kas mingi vaartus on oige
+    - kas midagi sisestati
+    - mis tuupi on sisend 
+    - kas sisestatud vaartus on mingis hulgas 
---
+++ code/06/app.js
@@ -0,0 +1,42 @@
+/* const tvs = [
+    [86, 'Android', 100, 40],
+    [52, 'Apple', 89, 30],
+    [62, 'Samsung', 120, 35],
+];
+
+/* for (let i= 0; i< tvs.lenght; i++) {
+console.log(tvs[i[3]]);}
+
+) */
+
+/* const tv = {
+    diagonal: 86,
+    opSystem: 'Android',
+    powerConsumption: 100,
+    weight: 40,
+};
+
+const user = {
+    email: 'brs@tlu.ee',
+    lastname: 'Sh',
+    yearOfBirth: 1990,
+};
+
+console.log (tv.diagonal); */
+
+const tvs = [{
+    diagonal: 86,
+    opSystem: 'Android',
+    powerConsumption: 100,
+    weight: 40,},
+    {
+    diagonal: 90,
+    opSystem: 'Apple',
+    powerConsumption: 110,
+    weight: 50,},
+    {
+    diagonal: 100,
+    opSystem: 'Samsung',
+    powerConsumption: 200,
+    weight: 60,},
+];
